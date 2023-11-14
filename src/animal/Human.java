678fc1251875429811c92380b5c9f752ab118fc8
JohnDoe2525 <naotakenoguchi@gmail.com>
2023-11-15 00:36:41 +0900

    add Review04

---
+++ src/animal/Human.java
@@ -0,0 +1,28 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+    private String hobby;
+
+    public Human(String name,int age,String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public void say() {
+        System.out.println(super.getName() + "です。" + super.getAge() + "歳です。");
+    }
+    
+    @Override
+    public void think() {
+        System.out.println("私は" + getHobby() + "について考えています。");
+    }
+}
